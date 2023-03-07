bda62a416e728631843bbb2a85052b37f3a65921
Parth119 <bhatiaparth119@gmail.com>
2023-03-07 09:59:39 +0530

    Added intial files

---
+++ FTP_Client/src/test/java/com/htrap/FTP_Client/FtpClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htrap.FTP_Client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtpClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ FTP_Server/src/test/java/com/htrap/FTP_Server/FtpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htrap.FTP_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
